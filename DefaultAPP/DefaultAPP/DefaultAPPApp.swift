f8f5169ed26afe255a779ce21ae0398d3c46d49b
hashtagEngineer <rushikeshsuradkar2000@gmail.com>
2023-07-27 11:25:44 +0530

    Default app first commit

---
+++ DefaultAPP/DefaultAPP/DefaultAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultAPPApp.swift
+//  DefaultAPP
+//
+//  Created by Rushikesh Suradkar on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DefaultAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
